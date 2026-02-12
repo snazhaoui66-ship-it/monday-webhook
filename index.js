af8f6fb27307a29d1fd5ad24fc218fdd8e718f6c
Said Nazhaoui <snazhaoui66@gmail.com>
2026-02-12 10:25:17 +0100

    Add middleware global

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ if (!MONDAY_API_KEY || !BOARD_ID) {
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("🌍 GLOBAL REQUEST:", req.method, req.url);
+  next();
+});
+
+
+
 // =========================
 // COLD START LOGGER
 // =========================
